a30528e7b88ee8de0e4807815d2a0f8566b95e90
mingodad@gmail.com <mingodad@gmail.com@1b650d05-8715-7df7-5a1d-7d16fbe59f41>
2012-07-04 08:50:51 +0000

    Initial release

---
+++ sqlite3-lua-regex.c
@@ -0,0 +1,20 @@
+#include "sqlite3.h"
+
+static void sqlite3_regexp( sqlite3_context *context, int argc, sqlite3_value **argv ) {
+    assert(argc == 2);
+    LuaMatchState ms;
+    const char *pattern = (const char *)sqlite3_value_text(argv[0]);
+    const char *subject = (const char *)sqlite3_value_text(argv[1]);
+    if(str_find(&ms, pattern, strlen(pattern), subject, strlen(subject), 0, 0))
+        sqlite3_result_int(context, 1);
+    else
+    {
+        if(ms.error) sqlite3_result_error(context, ms.error, strlen(ms.error));
+        else sqlite3_result_int(context, 0);
+    }
+}
+
+void set_sqlite3_regexp_func(sqlite3 *db)
+{
+    sqlite3_create_function(db,  "regexp", 2, SQLITE_UTF8, 0, sqlite3_regexp, 0, 0);
+}
