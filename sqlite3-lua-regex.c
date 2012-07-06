1fcb6da74f583904809e8ad6d0e2bcf97c610317
mingodad@gmail.com <mingodad@gmail.com@1b650d05-8715-7df7-5a1d-7d16fbe59f41>
2012-07-06 11:31:46 +0000

    add match function and change tue order of parameters to work as "subect match/regexp pattern"

--- sqlite3-lua-regex.c
+++ sqlite3-lua-regex.c
@@ -4,8 +4,8 @@
 static void sqlite3_regexp( sqlite3_context *context, int argc, sqlite3_value **argv ) {
     assert(argc == 2);
     LuaMatchState ms;
-    const char *pattern = (const char *)sqlite3_value_text(argv[0]);
-    const char *subject = (const char *)sqlite3_value_text(argv[1]);
+    const char *pattern = (const char *)sqlite3_value_text(argv[1]);
+    const char *subject = (const char *)sqlite3_value_text(argv[0]);
     if(str_find(&ms, pattern, strlen(pattern), subject, strlen(subject), 0, 0))
         sqlite3_result_int(context, 1);
     else
@@ -15,7 +15,9 @@ static void sqlite3_regexp( sqlite3_context *context, int argc, sqlite3_value **
     }
 }
 
-void set_sqlite3_regexp_func(sqlite3 *db)
+int set_sqlite3_regexp_func(sqlite3 *db)
 {
-    sqlite3_create_function(db,  "regexp", 2, SQLITE_UTF8, 0, sqlite3_regexp, 0, 0);
+  int nErr = sqlite3_create_function(db,  "regexp", 2, SQLITE_UTF8, 0, sqlite3_regexp, 0, 0);
+  nErr += sqlite3_create_function(db,  "match", 2, SQLITE_UTF8, 0, sqlite3_regexp, 0, 0);
+	return nErr ? SQLITE_ERROR : SQLITE_OK;
 }
